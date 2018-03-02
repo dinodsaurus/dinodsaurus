e4d412c71f2adb42be88beacab7d5eadf1007ab6
Dino Trojak <dinotrojak@gmail.com>
2018-03-02 13:17:55 +0100

    added paralax scroller

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,74 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax'
+
+import Layer1 from "../img/1.png";
+import Layer2 from "../img/2.png";
+import Layer3 from "../img/3.png";
+import Layer4 from "../img/4.png";
+import Layer5 from "../img/5.png";
+import Layer6 from "../img/6.png";
+import Layer7 from "../img/7.png";
+import Layer8 from "../img/8.png";
+import Layer9 from "../img/9.png";
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ParallaxProvider>
+      <ParallaxBanner
+        className="Head"
+        layers={[
+            {
+                image: Layer9,
+                amount: 0,
+                slowerScrollRate: false,
+            },
+            {
+                image: Layer8,
+                amount: 0,
+                slowerScrollRate: false,
+            },
+            {
+                image: Layer7,
+                amount: 0.5,
+                slowerScrollRate: false,
+            },
+            {
+                image: Layer6,
+                amount: 0.4,
+                slowerScrollRate: false,
+            },
+            {
+                image: Layer5,
+                amount: 0.2,
+                slowerScrollRate: false,
+            },
+            {
+                image: Layer4,
+                amount: 0.1,
+                slowerScrollRate: false,
+            },
+            {
+              image: Layer3,
+              amount: 0.2,
+              slowerScrollRate: false,
+          },
+          {
+            image: Layer2,
+            amount: 0.05,
+            slowerScrollRate: true,
+        },
+        {
+          image: Layer1,
+          amount: 0.2,
+          slowerScrollRate: true,
+      },
+        ]}
+        style={{
+            height: '700',
+        }}
+    />
+  </ParallaxProvider>
   </div>
 )
 
