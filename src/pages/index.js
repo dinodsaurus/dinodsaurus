7ef074dd345fa93fafc96770eab9a423fa717742
Dino Trojak <dinotrojak@gmail.com>
2018-03-15 11:44:37 +0100

    switched to css grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,30 +14,13 @@ const IndexPage = ({ data }) => {
       <Header />
       <div className="container">
         <PageTitle />
-        <div style={{
-          width: "100%",
-          display: "flex",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: "50px"
-        }}
-        >
+        <div className="grid">
           <BlogPost img="https://p.vitalmtb.com/photos/users/2/slideshows/9999/photos/22934/s1200_slideshow_photo_1460157605.jpg" />
           <BlogPost noBg img="https://www.wikihow.com/images/thumb/7/74/Write-a-Confusing-Code-Step-1.jpg/aid319822-v4-728px-Write-a-Confusing-Code-Step-1.jpg.webp" />
-          <BlogPost noBg light img="http://rajsuvd8yc-flywheel.netdna-ssl.com/wp-content/uploads/2016/05/DSC_0286-1-e1499891304192-705x293.jpg" />
-        </div>
-        <div style={{
-          width: "100%",
-          display: "flex",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: "25px"
-        }}
-        >
+          <BlogPost img="http://rajsuvd8yc-flywheel.netdna-ssl.com/wp-content/uploads/2016/05/DSC_0286-1-e1499891304192-705x293.jpg" />
           <BlogPost img="https://p.vitalmtb.com/photos/users/2/slideshows/9999/photos/22934/s1200_slideshow_photo_1460157605.jpg" />
           <BlogPost noBg light long img="http://rajsuvd8yc-flywheel.netdna-ssl.com/wp-content/uploads/2016/05/DSC_0286-1-e1499891304192-705x293.jpg" />
+
         </div>
       </div>
       <Footer />
