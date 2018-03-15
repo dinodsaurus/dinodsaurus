f1387d8d13bf08d83aa6f9a5de78b1559f8fd89a
Dino Trojak <dinotrojak@gmail.com>
2018-03-15 11:48:04 +0100

    added margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,8 @@ const IndexPage = ({ data }) => {
           <BlogPost img="https://p.vitalmtb.com/photos/users/2/slideshows/9999/photos/22934/s1200_slideshow_photo_1460157605.jpg" />
           <BlogPost noBg img="https://www.wikihow.com/images/thumb/7/74/Write-a-Confusing-Code-Step-1.jpg/aid319822-v4-728px-Write-a-Confusing-Code-Step-1.jpg.webp" />
           <BlogPost img="http://rajsuvd8yc-flywheel.netdna-ssl.com/wp-content/uploads/2016/05/DSC_0286-1-e1499891304192-705x293.jpg" />
-          <BlogPost img="https://p.vitalmtb.com/photos/users/2/slideshows/9999/photos/22934/s1200_slideshow_photo_1460157605.jpg" />
           <BlogPost noBg light long img="http://rajsuvd8yc-flywheel.netdna-ssl.com/wp-content/uploads/2016/05/DSC_0286-1-e1499891304192-705x293.jpg" />
+          <BlogPost img="https://p.vitalmtb.com/photos/users/2/slideshows/9999/photos/22934/s1200_slideshow_photo_1460157605.jpg" />
 
         </div>
       </div>
