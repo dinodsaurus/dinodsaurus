9b736ee8f4c69eb6c716a8f26f2fea12cb70e077
Dino Trojak <dinotrojak@gmail.com>
2018-06-19 09:52:45 +0200

    added favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,11 @@ import "reset-css";
 
 import "./index.css";
 
+import appleTouch from "../img/favicon/apple-touch-icon.png";
+import favicon32 from "../img/favicon/favicon-32x32.png";
+import favicon16 from "../img/favicon/favicon-16x16.png";
+import safariPinned from "../img/favicon/safari-pinned-tab.svg";
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
@@ -12,6 +17,9 @@ const TemplateWrapper = ({ children }) => (
       <meta name="description" content="Dino Trojak's lair" />
       <meta name="keywords" content="JavaScript, MTB, JiuJitsu, YT Jeffsy" />
       <link rel="stylesheet" href="https://fonts.typotheque.com/WF-026590-010248.css" type="text/css" />
+      <link rel="apple-touch-icon" sizes="180x180" href={appleTouch} />
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
     </Helmet>
     <div>
       {children()}
