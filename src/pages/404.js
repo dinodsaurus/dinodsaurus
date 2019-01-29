a29d2fbb6a74e319c5d083de232a496c5272322a
Dino Trojak <dinotrojak@gmail.com>
2019-01-29 16:45:01 +0100

    added lost page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,33 @@
 import React from "react";
+import Helmet from "react-helmet";
+import Link from "gatsby-link";
+
+import Header from "../components/Header";
+import compass from "../img/compass.png";
+
+import styles from "./about.module.css";
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div>
+      <Helmet>
+        <title>Dino Trojak | You are Lost :S</title>
+      </Helmet>
+      <Header />
+      <div className={styles.containerLost}>
+        <img className={styles.icon} src={compass} alt="compass" />
+        <h1 className={styles.titleLost}>
+          It seems that you are lost
+          {" "}
+          <span role="img">😱</span>
+          {" "}
+          <br />
+          <Link className={styles.link} to="/">
+            Please go back to the Home Page
+          </Link>
+        </h1>
+      </div>
+    </div>
   </div>
 );
 
